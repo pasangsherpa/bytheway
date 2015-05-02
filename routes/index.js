953f65803640f6681c78676ffaa41631bf382992
Pasang Sherpa <pgsherpa12@gmail.com>
2015-05-02 15:59:19 -0700

    validate it's twill

--- routes/index.js
+++ routes/index.js
@@ -24,43 +24,41 @@ router.get('/', function(req, res, next) {
 
 router.post('/image', function(req, res) {
     // Validate Twilio req
-    // if (twilio.validateExpressRequest(req, config.authToken)) {
+    if (twilio.validateExpressRequest(req, process.env.TWILIO_TOKEN)) {
+        var image = req.body;
+        var imageName = image.From.replace('+', '') + '/' + image.SmsMessageSid + '.jpg';
 
-    var image = req.body;
-    var imageName = image.From.replace('+', '') + '/' + image.SmsMessageSid + '.jpg';
-
-    gm(request(image.MediaUrl0), imageName)
-        .resize(null, 500)
-        .compress('Lossless')
-        .noProfile()
-        .stream(function(err, stdout, stderr) {
-            if (err) console.log(err);
-            var buf = new Buffer('');
-            stdout.on('data', function(data) {
-                buf = Buffer.concat([buf, data]);
-            });
-            stdout.on('end', function() {
-                var data = {
-                    Bucket: 'disruptny',
-                    Key: imageName,
-                    Body: buf,
-                    ACL: 'public-read',
-                    ContentType: mime.lookup(imageName)
-                };
-                S3.putObject(data, function(err, response) {
-                    if (err) console.log(err);
-                    db.insertImage({
-                        image: 'https://s3.amazonaws.com/disruptny/' + imageName,
-                        phoneNumber: image.From
+        gm(request(image.MediaUrl0), imageName)
+            .resize(null, 500)
+            .compress('Lossless')
+            .noProfile()
+            .stream(function(err, stdout, stderr) {
+                if (err) console.log(err);
+                var buf = new Buffer('');
+                stdout.on('data', function(data) {
+                    buf = Buffer.concat([buf, data]);
+                });
+                stdout.on('end', function() {
+                    var data = {
+                        Bucket: 'disruptny',
+                        Key: imageName,
+                        Body: buf,
+                        ACL: 'public-read',
+                        ContentType: mime.lookup(imageName)
+                    };
+                    S3.putObject(data, function(err, response) {
+                        if (err) console.log(err);
+                        db.insertImage({
+                            image: 'https://s3.amazonaws.com/disruptny/' + imageName,
+                            phoneNumber: image.From
+                        });
+                        res.status(200).send('OK');
                     });
-                    res.status(200).send('OK');
                 });
             });
-        });
-
-    // } else {
-    //   return res.send('Nice try imposter.');
-    // }
+    } else {
+        return res.send('Nice try imposter.');
+    }
 });
 
 module.exports = router;
