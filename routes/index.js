927ad68360cd28e86247bb9501052b3dbbccb925
Pasang Sherpa <pgsherpa12@gmail.com>
2015-05-02 13:51:29 -0700

    content-length error fix

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var uploadFile = require('../uploadFile.js');
+var request = require('request');
 var AWS = require('aws-sdk');
 var twilio = require('twilio');
 var AWS = require('aws-sdk');
 var S3 = new AWS.S3();
-var gm = require('gm');
+var gm = require('gm').subClass({
+    imageMagick: true
+});
 var mime = require('mime');
 
 router.get('/s3upload', function(req, res, next) {
@@ -25,21 +28,27 @@ router.post('/image', function(req, res) {
     var image = req.body;
     console.log(req.body);
 
-    var imageName = image.From + '/' + SmsMessageSid + '.jpg';
+    var imageName = image.From + '/' + image.SmsMessageSid + '.jpg';
 
     gm(request(image.MediaUrl0), imageName)
         .resize('100^', '100^')
         .stream(function(err, stdout, stderr) {
-            var data = {
-                Bucket: 'bytheway',
-                Key: imageName,
-                Body: stdout,
-                ACL: 'public-read',
-                ContentType: mime.lookup(imageName)
-            };
-            S3.client.putObject(data, function(err, res) {
-                console.log('imageName: ' + imageName);
-                res.status(200).send('OK');
+            var buf = new Buffer('');
+            stdout.on('data', function(data) {
+                buf = Buffer.concat([buf, data]);
+            });
+            stdout.on('end', function(data) {
+                var data = {
+                    Bucket: 'disruptny',
+                    Key: imageName,
+                    Body: buf,
+                    ContentType: mime.lookup(imageName)
+                };
+                S3.putObject(data, function(err, res) {
+                    if (err) console.log(err);
+                    console.log('imageName: ' + imageName);
+                    res.status(200).send('OK');
+                });
             });
         });
 
