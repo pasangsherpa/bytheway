e931a1e778437b09988136df766f1de1e7fa3f60
mllona <llona.melanie@gmail.com>
2015-05-02 15:30:28 -0400

    Upload test photo to AWS S3 bucket

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var uploadFile = require('../uploadFile.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.json("HELLO")
+    res.json("HELLO");
+});
+
+router.get('/s3upload', function(req, res, next) {
+	uploadFile.sendToS3();
+	res.json("Upload success?");
 });
 
 module.exports = router;
