2beda64d8ecd534e379c3d69ec33ceca57494530
Pasang Sherpa <pgsherpa12@gmail.com>
2015-05-02 11:59:03 -0700

    twilio, was, imagemagick package added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
+var twilio = require('twilio');
+var S3 = new AWS.S3();
+var gm = require('gm');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.json("HELLO")
+    res.status(200).send('By the way you can text us an image @ 920-THE-WAY0');
+});
+
+router.post('/image', function(req, res) {
+   // Validate Twilio req
+  // if (twilio.validateExpressRequest(req, config.authToken)) {
+
+    console.log(req.body)
+    res.status(200).send('OK');
+
+  // } else {
+  //   return res.send('Nice try imposter.');
+  // }
 });
 
 module.exports = router;
