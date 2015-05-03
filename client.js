4b6a3a7642c52588b00d9161b2ffbd7217be31d8
Ryan Ewing <ryan.ewing@concur.com>
2015-05-02 18:08:59 -0700

    fixes going past the queue limit

--- client.js
+++ client.js
@@ -12,7 +12,7 @@ var imageQueue = [
 // 3. fade in the image
 
 function insertImage(url) {
-  var $newImageContainer = $('<div style="height:0px; border:1px solid black; width:344px"></div>');
+  var $newImageContainer = $('<div class="item" style="height:0px; border:1px solid black; width:344px"></div>');
   $container.prepend($newImageContainer);
   $newImageContainer.animate({height: "233px"}, 500, function() {
     var $newImage = $('<img src="'+url+'" style="display:none"/>');
@@ -41,8 +41,12 @@ function insertImageListener() {
 }
 
 function insertTimer() {
-  ee.emit('insertImage');
-  setTimeout(insertTimer,5000);
+  if (isQueueEmpty()) {
+    // grab some photos from the end and insert them
+  } else {
+    ee.emit('insertImage');
+    setTimeout(insertTimer,5000);
+  }
 }
 
 setTimeout(insertTimer, 5000);
@@ -51,7 +55,12 @@ ee.addListener('insertImage', insertImageListener);
 
 function isQueueEmpty() {return imageQueue.length === 0}
 
+function popLastPhoto() {
+  var items = $(".item");
+  var last = items[items.length - 1];
 
+  var src = $(last).children()[0].src;
+}
 
 
 
